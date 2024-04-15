76d1457c8b4300f2bc3b9dc3cde87bfa19edcd88
Anesu Napsta <napstakid@gmail.com>
2024-04-15 23:35:40 +0200

    Add:Input Sample

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.mount("/payroll", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/")
 async def root():
@@ -14,19 +15,15 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-# def generate_html_response():
-#     html_content = """
-#     <html>
-#         <head>
-#             <title>Some HTML in here</title>
-#         </head>
-#         <body>
-#             <h1>Look ma! HTML!</h1>
-#         </body>
-#     </html>
-#     """
-#     return HTMLResponse(content=html_content, status_code=200)
-
-# @app.get("/payroll", response_class=HTMLResponse)
-# async def read_items():
-#     return generate_html_response()
+@app.get("/payroll/", response_class=HTMLResponse)
+async def renderHtml():
+    return """
+    <html>
+        <head>
+            <title>Payroll</title>
+        </head>
+        <body>
+            <h1>Payroll</h1>
+        </body>
+    </html>
+    """
