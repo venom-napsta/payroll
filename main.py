31258bd31d6b67be49cca2bf2b221b1994f2f0ac
Anesu Napsta <napstakid@gmail.com>
2024-04-15 21:53:40 +0200

    Add: Static Files

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
+app.mount("/payroll", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
@@ -14,19 +14,19 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-def generate_html_response():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+# def generate_html_response():
+#     html_content = """
+#     <html>
+#         <head>
+#             <title>Some HTML in here</title>
+#         </head>
+#         <body>
+#             <h1>Look ma! HTML!</h1>
+#         </body>
+#     </html>
+#     """
+#     return HTMLResponse(content=html_content, status_code=200)
 
-@app.get("/payroll", response_class=HTMLResponse)
-async def read_items():
-    return generate_html_response()
+# @app.get("/payroll", response_class=HTMLResponse)
+# async def read_items():
+#     return generate_html_response()
