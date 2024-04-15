094a1e269bbb26b66e7726980f0762097684bfc2
Anesu Ruzvidzo <49985091+venom-napsta@users.noreply.github.com>
2024-04-15 15:32:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from fastapi.responses import HTMLResponse
 
 from fastapi import FastAPI
 
@@ -12,3 +13,20 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+def generate_html_response():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/payroll", response_class=HTMLResponse)
+async def read_items():
+    return generate_html_response()
